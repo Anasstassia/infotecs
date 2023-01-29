660c33b7895ef1d99a0e79cde588f52c6947e4f4
Anasstassia <gubinaa75@gmail.com>
2023-01-29 17:50:05 +0400

    feat: add table render

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 import data from "./data.json" assert { type: "json" };
+
+const state = data.map(el => ({ ...el, firstName: el.name.firstName, lastName: el.name.lastName }));
+
+const selectElement = (key) => `
+    <select name="options" data-key="${key}" id="select">
+        <option value="none">-Выбрать-</option>
+        <option value="increase">Сортировка от А до Я</option>
+        <option value="decrease">Сортировка от Я до А</option>
+        <option value="hide">Скрыть столбец</option>
+    </select>`;
+
+const renderData = () => {
+    document.querySelector('#tbody').innerHTML =  state.map((el) =>
+        `<tr class="row" data-id="${el.id}">
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td class="td_about">${el.about}</td>
+        <td>${el.eyeColor}</td>
+        </tr>`
+    ).join('');
+};
+
+const renderTable = () => {
+    const html = `
+        <table class="table" cellspacing="0" border="0" cellpadding="0">
+        <thead>
+        <th>Имя ${selectElement('firstName')}</th>
+        <th>Фамилия ${selectElement('lastName')}</th>
+        <th>Описание ${selectElement('about')}</th>
+        <th>Цвет глаз ${selectElement('eyeColor')}</th>
+        </thead>
+        <tbody id="tbody">
+        </tbody>
+        </table>
+    `;
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    document.body.innerHTML = div.innerHTML;
+    renderData();
+};
+
+renderTable();
